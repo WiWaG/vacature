44d62fa37eabb91e5b1c2d6612ca1468823f775e
WiWaG <willem@wiwaweb.nl>
2021-01-14 09:56:32 +0100

    conv framework

--- index.php
+++ index.php
@@ -1,9 +1,51 @@
-<?php include_once 'config/init.php';  ?>
-
 <?php
 
-$template = new Template('templates/frontpage.php');
 
-$template->title = 'Dashboard';
 
-echo $template;
+use lib\Router;
+
+use lib\Request;
+
+
+
+require 'Core/bootstrap.php';
+
+
+
+$route = Router::load('routes.php')->direct(Request::uri(), Request::method());
+
+
+
+require $route['uri'];
+
+$class = new $route['class'];
+
+
+
+$function = $route['function'];
+
+
+
+if (!Request::ajax())
+
+{
+
+    // Load the HTML header
+
+        require 'views/layouts/head.view.php';
+
+
+
+            // Inject code from contho $class->$function();
+
+
+
+                // Close it with the bottom end </body> and </html> tags
+
+                    require 'views/layouts/bottom.view.php';
+
+                    } else {
+
+                        echo $class->$function();
+
+                        }
